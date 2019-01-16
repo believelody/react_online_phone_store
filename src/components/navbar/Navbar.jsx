b824f66dd0b45533151e997f5a80748c2bdf744a
believelody <believelody@gmail.com>
2019-01-16 19:34:52 +0100

    Added navbar, product-list, product-detail and cart boilerplate components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>From Navbar</h3>
+      </div>
+    )
+  }
+}
+
+export default Navbar
